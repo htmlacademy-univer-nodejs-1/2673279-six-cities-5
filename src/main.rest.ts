45055a1a49d57ca4d74f4c3a0dfaf13b455e0754
Лагутин Денис Алексеевич <d.lagutin@nir.center>
2025-10-27 17:51:50 +0500

    до 3-его

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -4,6 +4,7 @@ import { RestApplication } from './rest/index.js';
 import { Component } from './shared/types/index.js';
 import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
+import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
 
 async function bootstrap() {
   const container = new Container();
@@ -12,6 +13,8 @@ async function bootstrap() {
   container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
 
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
+
+  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
 
   const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -1,28 +1,62 @@
 import convict from 'convict';
+import validator from 'convict-format-with-validator';
+
+convict.addFormats(validator);
 
 export interface RestSchema {
   PORT: number;
-  SALT: string;
+  SALT: string | null; 
   DB_HOST: string;
+  DB_USER: string;
+  DB_PASSWORD: string;
+  DB_PORT: number;
+  DB_NAME: string;
 }
 
 export const restSchema = convict<RestSchema>({
   PORT: {
-    doc: 'Порт для входящих соединений',
+    doc: 'Port for incoming connections',
     format: 'port',
     env: 'PORT',
     default: 4000
   },
   SALT: {
-    doc: 'Соль для хеширования паролей',
+    doc: 'Salt for password hashing',
     format: String,
     env: 'SALT',
-    default: null
+    default: null 
   },
   DB_HOST: {
-    doc: 'Хост базы данных',
-    format: String,
+    doc: 'IP address of the database server',
+    format: 'ipaddress',
     env: 'DB_HOST',
-    default: 'localhost'
+    default: '127.0.0.1'
+  },
+  DB_USER: {
+    doc: 'Database user',
+    format: String,
+    env: 'DB_USER',
+    default: ''
+  },
+  DB_PASSWORD: {
+    doc: 'Database password',
+    format: String,
+    env: 'DB_PASSWORD',
+    default: ''
+  },
+  DB_PORT: {
+    doc: 'Database port',
+    format: 'port',
+    env: 'DB_PORT',
+    default: 27017
+  },
+  DB_NAME: {
+    doc: 'Database name',
+    format: String,
+    env: 'DB_NAME',
+    default: 'six-cities'
   }
 });
+
+
+export type ConfigSchema = typeof restSchema;
