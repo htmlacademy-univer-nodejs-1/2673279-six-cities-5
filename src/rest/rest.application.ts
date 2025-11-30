8b59a82ad0d5101b7226e6c75a1bd31af28d5ab8
Денис <denislagutin8791@gmail.com>
2025-11-30 16:36:35 +0500

    первый шаг

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -19,6 +19,7 @@ export class RestApplication {
     @inject(Component.UserController) private readonly userController: Controller,
     @inject(Component.OfferController) private readonly offerController: Controller,
     @inject(Component.FavoritesController) private readonly favoritesController: Controller,
+    @inject(Component.CommentController) private readonly commentController: Controller,
   ) {
     this.server = express();
   }
@@ -51,6 +52,7 @@ export class RestApplication {
     this.server.use('/users', this.userController.router);
     this.server.use('/offers', this.offerController.router);
     this.server.use('/favorites', this.favoritesController.router);
+    this.server.use('/comments', this.commentController.router);
   }
 
   private async _initExceptionFilters() {
